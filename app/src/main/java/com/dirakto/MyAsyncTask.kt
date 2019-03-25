5f0ac833ec8d38d9e6245e8fdfe83b95c73d15e8
Oskar Rajzer <s15342@pjwstk.edu.pl>
2019-03-26 00:40:20 +0100

    Commit 2

---
+++ app/src/main/java/com/dirakto/MyAsyncTask.kt
@@ -0,0 +1,40 @@
+package com.dirakto
+
+import android.graphics.Canvas
+import android.os.AsyncTask
+import android.util.Log
+import android.view.SurfaceHolder
+
+class MyAsyncTask: AsyncTask<Any, Any, Any> {
+
+    var holder: SurfaceHolder? = null
+    var view: MySurfaceView? = null
+    var canvas: Canvas? = null
+    var notCancelled: Boolean = true
+
+    val FPS: Long = 30
+
+    constructor(holder: SurfaceHolder, view: MySurfaceView): super(){
+        this.holder = holder
+        this.view = view
+    }
+
+    override fun doInBackground(vararg params: Any?): Any {
+        while(isCancelled){
+            publishProgress()
+            Thread.sleep(1000L/FPS)
+
+        }
+        return "Done"
+    }
+
+    override fun onProgressUpdate(vararg values: Any?) {
+        Log.i("MyINFO", "DZIALAM")
+        canvas = holder?.lockCanvas()
+//        view?.update()
+        view?.draw(canvas)
+        holder?.unlockCanvasAndPost(canvas)
+    }
+    override fun onPreExecute() {
+    }
+}
--- app/src/main/java/com/dirakto/MySurfaceView.kt
+++ app/src/main/java/com/dirakto/MySurfaceView.kt
@@ -1,39 +1,79 @@
 package com.dirakto
 
 import android.content.Context
-import android.graphics.Canvas
+import android.content.res.Resources
+import android.graphics.*
+import android.os.AsyncTask
+import android.util.AttributeSet
+import android.util.Log
 import android.view.SurfaceHolder
 import android.view.SurfaceView
 
-class MySurfaceView(context: Context): SurfaceView(context), SurfaceHolder.Callback{
+class MySurfaceView: SurfaceView, SurfaceHolder.Callback{
 
-    var myThread: MyThread? = null
+    var myThread: MyAsyncTask? = null
 
-
-    init{
-        holder.addCallback(this)
-//        myThread = MyThread()
-
-//        this.setFocusable(true)
-
-
+    constructor(context: Context): super(context){
+        this.holder.addCallback(this)
+        this.setFocusable(true)
+        setWillNotDraw(false)
     }
-
+    constructor(context: Context, attSet: AttributeSet): super(context, attSet){
+        this.holder.addCallback(this)
+        this.setFocusable(true)
+        setWillNotDraw(false)
+    }
+    constructor(context: Context, attSet: AttributeSet, int: Int): super(context, attSet, int){
+        this.holder.addCallback(this)
+//        myThread = MyAsyncTask()
+        this.setFocusable(true)
+        setWillNotDraw(false)
+    }
+    constructor(context: Context, attSet: AttributeSet, int1: Int, int2: Int): super(context, attSet, int1, int2){
+        this.holder.addCallback(this)
+//        myThread = MyAsyncTask()
+        this.setFocusable(true)
+        setWillNotDraw(false)
+    }
 
 
     override fun surfaceChanged(holder: SurfaceHolder?, format: Int, width: Int, height: Int) {}
 
-    override fun surfaceDestroyed(holder: SurfaceHolder?) {}
+    override fun surfaceDestroyed(holder: SurfaceHolder?) {
+//        Log.i()
+//        myThread?.notCancelled = false;
+//        myThread?.cancel(true)
+    }
 
     override fun surfaceCreated(holder: SurfaceHolder?) {
+        myThread = MyAsyncTask(this.holder, this)
+            myThread?.execute()
+//        holder?.apply {
+//            val canvas = lockCanvas()
+//
+//            canvas.drawCircle(100f, 100f, 100f, Paint())
+//            unlockCanvasAndPost(canvas)
+//        }
+//        this.requestLayout()
+
 
     }
-    fun update(){
 
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+            canvas?.drawColor(Color.WHITE)
+
+            val oldBitmap = BitmapFactory.decodeResource(resources, R.drawable.tomaszew_bird32_b)
+            val scale = 1
+            val bitmap = Bitmap.createScaledBitmap(oldBitmap, oldBitmap.width / scale, oldBitmap.height / scale, true)
+            var matrix = Matrix()
+            matrix.setRotate(50f, bitmap.width / 2f, bitmap.height / 2f)
+            canvas?.drawBitmap(bitmap, matrix, Paint())
     }
 
-//    override fun draw(canvas: Canvas?) {
-//        super.draw(canvas)
+//    fun update(){
+//
 //    }
 
 }
